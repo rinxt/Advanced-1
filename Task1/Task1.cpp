8313b17fea5f8e6e834eac827a81973c3de85e70
Мамаева Арина Евгеньевна <m18282@kometadom.local>
2023-09-11 17:13:26 +0300

    Task1 added

---
+++ Task1/Task1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    system("chcp 1251");
+    std::vector<int> multiply_vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    std::cout << "Входные данные: ";
+    for (const int& num : multiply_vec) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::for_each(multiply_vec.begin(), multiply_vec.end(), [](int& num) {
+        if (num % 2 != 0) {
+            num *= 3;
+        }
+        });
+
+    std::cout << "Выходные данные: ";
+    for (const int& num : multiply_vec) {
+        std::cout << num << " ";
+    }
+}
